ced7319b9da761cb364556a96e9d98982a7d7f37
mutharajuvamsikrishna <130433935+mutharajuvamsikrishna@users.noreply.github.com>
2023-09-20 17:36:28 +0530

    Initial commit

---
+++ src/main/java/com/web/controller/EmpController.java
@@ -0,0 +1,33 @@
+package com.web.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.web.model.Pro;
+import com.web.repo.ProRepo;
+import com.web.repo.RegisterRepo;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:5173")
+public class EmpController {
+
+	@Autowired
+	private ProRepo repo1;
+
+	@Autowired
+	private RegisterRepo repo5;
+
+	@PostMapping("/prosave") // save professional details
+	public String profesionalsave(@RequestBody Pro emp, Model model) {
+
+		repo1.save(emp);
+
+		return "personaldetails";
+
+	}
+
+}
---
+++ src/main/java/com/web/controller/ProController.java
@@ -0,0 +1,47 @@
+package com.web.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.web.model.Pro;
+import com.web.repo.ProRepo;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:5173")
+public class ProController {
+
+	@Autowired
+	private ProRepo proRepo;
+	@Autowired
+	private ProRepo repo1;
+
+	@GetMapping("/req")
+
+	public List<Pro> getPros() {
+		return repo1.findAll();
+	}
+
+	@GetMapping("/search")
+
+	public List<Pro> searchPro(@RequestParam("query") String query, Model model) {
+		List<Pro> users1 = proRepo.searchPro(query);
+		model.addAttribute("users1", users1);
+		return users1;
+	}
+
+	@DeleteMapping(value = "/delete") // edited /per455
+	public String delete(@RequestParam Long id) {
+		repo1.deleteById(id);
+
+		return "Deleted Sucess Fully";
+
+	}
+
+}
