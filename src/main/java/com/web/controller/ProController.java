72fd493ff2b7ba81069af067c2e43d122f859e0a
mutharajuvamsikrishna <130433935+mutharajuvamsikrishna@users.noreply.github.com>
2023-11-16 17:04:46 +0530

    Update User Edit,Admin Edit Api

--- src/main/java/com/web/controller/ProController.java
+++ src/main/java/com/web/controller/ProController.java
@@ -1,12 +1,15 @@
 package com.web.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -42,6 +45,16 @@ public class ProController {
 
 		return "Deleted Sucess Fully";
 
+	}
+
+	@PutMapping("/usereditupdate")
+	public String updateUserEdit(@RequestBody Pro pro) {
+		Long id = pro.getId();
+		Optional<Pro> pro1 = repo1.findById(id);
+		if (pro1 != null) {
+			repo1.save(pro);
+		}
+		return "updated successfullly";
 	}
 
 }
